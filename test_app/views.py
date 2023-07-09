fce1d79014c24d5a929cff0152ed88a4cf174759
dimka2338 <dimka2338@ya.ru>
2023-07-09 17:22:40 +0300

    Выполнил первое задание. Написал функцию которая для каждого конкретного объекта строительства будет возвращать список только родительских секций. И посчитал бюджет для каждой секции

--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.template.context_processors import request
 
-# Create your views here.
+from test_app.models import Building, Section
+
+
+## Задание 1.
+# Написать тело функции, которая для каждого конкретного объекта строительства
+# будет возвращать список только родительских секций. У каждой родительской
+# секции необходимо посчитать бюджет (стоимость всех расценок внутри).
+
+def get_parent_sections(building_id: int) -> list[Section]:
+    parent_sections = Section.objects.filter(building_id=building_id, parent=None)
+
+    for section in parent_sections:
+        budget = 0
+        expenditures = section.expenditure_set.all()
+        for expenditure in expenditures:
+            budget += expenditure.count * expenditure.price
+        section.budget = budget
+    return parent_sections
+
